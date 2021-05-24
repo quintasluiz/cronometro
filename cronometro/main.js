1104dffea50c71ce3517141ad573788afe26431e
Luiz <luizquintas1@gmail.com>
2021-05-24 14:41:03 -0300

    cronometro

---
+++ cronometro/main.js
@@ -0,0 +1,38 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds'); 
+
+
+
+const newTrip = '1 Jan 2022';
+
+function countdown() {
+    const newYearTrip = new Date(newTrip);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearTrip - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+    function formatTime(time) {
+        return time < 10 ? `0${time}` : time;
+    }
+}
+
+
+
+
+//Initial call
+
+countdown();
+
+setInterval(countdown, 1000);
